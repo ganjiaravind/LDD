cc42c9c5e8c9e5c119f3f826373a3bff301aa671
Ganji Aravind <ganjiaravind@gmail.com>
2021-02-06 07:42:38 +0530

    LLD: Adding char dynamic driver example and test app
    Signed-off-by: Ganji Aravind <ganjiaravind@gmail.com>

---
+++ char/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define DEV_NAME	"/dev/LLD_Cdev"
+int main()
+{
+	int fd;
+	fd = open(DEV_NAME, O_RDWR);
+	if (fd < 0) {
+		printf("failed to open file %s\n", DEV_NAME);
+		return fd;
+	}
+	printf("successfully open device file %s\n", DEV_NAME);
+	write(fd, "Hello Dev", sizeof("Hello Dev"));
+	close(fd);
+
+	return 0;
+}
